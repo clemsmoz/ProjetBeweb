dcfbedb0ab027ecda265ec5b580b5a22873ba89d
clement mozin <clementmozin@gmail.com>
2023-06-23 12:05:14 +0200

    mise a jour

--- python-api/DAO/queries_admin_REMOTE_673.py
+++
@@ -1,7 +0,0 @@
-get_admin = "SELECT * FROM administrateur WHERE pseudoAdmin = %(pseudo)s;"
-
-insert_admin_formateur_false = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin, id_section_FK) values (%s, %s, %s, %s, %s, %s, %s)"
-
-insert_admin_formateur_true = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin, id_formateur_FK, id_section_FK) values (%s, %s, %s, %s, %s, %s, %s, %s)"
-
-select_admin_by_pseudo = "SELECT * FROM Administrateur WHERE pseudoAdmin = %s"
